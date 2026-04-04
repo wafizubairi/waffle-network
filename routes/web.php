3148a3c3b731be8287100063b63955c5a2b63e1b
wafizubairi <emanwifi123@gmail.com>
2026-04-04 22:28:22 +0800

    UPDATE LANGUAGE

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\LanguageController;
 
 Route::get('/', [PageController::class, 'home']);
 Route::get('/services', [PageController::class, 'services']);
 Route::get('/portfolio', [PageController::class, 'portfolio']);
 Route::get('/about', [PageController::class, 'about']);
+Route::get('/lang/{locale}', [LanguageController::class, 'swap'])->name('lang.swap');
 Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
